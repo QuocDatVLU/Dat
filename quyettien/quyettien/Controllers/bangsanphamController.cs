9a01281eef977f4836619f862849db8e612ffae2
datbeo <datbeo@gmail.com>
2018-11-28 12:17:47 +0700

    project

---
+++ quyettien/quyettien/Controllers/bangsanphamController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using quyettien.Models;
+using System.Transactions;
+namespace quyettien.Controllers
+{
+    public class bangsanphamController : Controller
+    {
+        private CS4PEEntities db = new CS4PEEntities();
+
+        // GET: /bangsanpham/
+        public ActionResult Index()
+        {
+            var bangsanphams = db.BangSanPhams.Include(b => b.LoaiSanPham);
+            return View(bangsanphams.ToList());
+        }
+
+        // GET: /bangsanpham/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BangSanPham bangsanpham = db.BangSanPhams.Find(id);
+            if (bangsanpham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bangsanpham);
+        }
+
+        // GET: /bangsanpham/Create
+        public ActionResult Create()
+        {
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai");
+            return View();
+        }
+
+        // POST: /bangsanpham/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create( BangSanPham model)
+        {
+            using (var scope = new TransactionScope())
+            {
+                db.BangSanPhams.Add(model);
+                db.SaveChanges();
+
+                var path = Server.MapPath("~/App_Data");
+                path = path + "/" + model.id;
+                if (Request.Files["HinhAnh"] != null && Request.Files["HinhAnh"].ContentLength > 0)
+                {
+                    Request.Files["HinhAnh"].SaveAs(path);
+
+                    scope.Complete();//approve for transaction
+                    return RedirectToAction("Index");
+                }
+                else
+                    ModelState.AddModelError("HinhAnh", "Chua co hinh SP");
+            }
+
+            Checkbangsanpham(model);
+            if (ModelState.IsValid)
+            {
+                db.BangSanPhams.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", model.Loai_id);
+            return View(model);
+        }
+        private void Checkbangsanpham(BangSanPham model)
+        {
+            if (model.GiaGoc < 0)
+                ModelState.AddModelError("GiaGoc", "Giá gốc phải lớn hơn 0!");
+            if (model.GiaBan < model.GiaGoc)
+                ModelState.AddModelError("GiaBan", "Giá bán phải lớn hơn giá gốc");
+        }
+
+        // GET: /bangsanpham/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BangSanPham bangsanpham = db.BangSanPhams.Find(id);
+            if (bangsanpham == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", bangsanpham.Loai_id);
+            return View(bangsanpham);
+        }
+
+        // POST: /bangsanpham/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="id,MaSP,TenSP,Loai_id,GiaBan,GiaGoc,GiaGop,SoLuongTon")] BangSanPham bangsanpham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bangsanpham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", bangsanpham.Loai_id);
+            return View(bangsanpham);
+        }
+
+        // GET: /bangsanpham/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BangSanPham bangsanpham = db.BangSanPhams.Find(id);
+            if (bangsanpham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bangsanpham);
+        }
+
+        // POST: /bangsanpham/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BangSanPham bangsanpham = db.BangSanPhams.Find(id);
+            db.BangSanPhams.Remove(bangsanpham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
